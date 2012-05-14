9aee5fcc9cb0fa155919f1ed823ea08c3e08ac78
jkyoung <jkyoung0@gmail.com>
2012-05-14 13:11:22 +0900

    test for .deb

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup (name = 'swift-cli',
        author = 'Kiyoung Jung',
        author_email = 'jkyoung0@gmail.com',
        url = 'https://github.com/jkyoung0/swift-cli-dist',
+       license = 'Apache License, Version 2.0',
+       platforms = 'pure python, v2.6 or v2.7 based',
        packages = ['swift-cli'],
        package_dir = {'swift-cli': 'src'},
 )
