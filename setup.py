3d33899029c55c42776c6562067f9a26e77cf824
jkyoung <jkyoung0@gmail.com>
2012-05-14 11:12:03 +0900

    update changes

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from distutils.core import setup
 setup (name = 'swift-cli',
        version = '1.4.2-1',
        description = 'swift command line interface',
+       author = 'Kiyoung Jung',
+       author_email = 'jkyoung0@gmail.com',
        url = 'https://github.com/jkyoung0/swift-cli-dist',
        packages = ['swift-cli'],
        package_dir = {'swift-cli': 'src'},
